7ed11b08cf7e9b5a394d87b93950a681a2ce5002
Roderick Kennedy <roderick.kennedy@gmail.com>
2014-02-20 16:43:43 +0000

    .

--- WidgetCollection.cpp
+++ WidgetCollection.cpp
@@ -5,14 +5,29 @@
 #include "QIntSliderEditInterface.h"
 
 #ifdef QT5
+	#ifdef _DEBUG
+		#pragma comment(lib,"Qt5Designerd")
+		#pragma comment(lib,"Qt5Cored")
+		#pragma comment(lib,"Qt5Guid")
+		#pragma comment(lib,"Qt5Widgetsd")
+	#else
 #pragma comment(lib,"Qt5Designer")
 #pragma comment(lib,"Qt5Core")
 #pragma comment(lib,"Qt5Gui")
 #pragma comment(lib,"Qt5Widgets")
+	#endif
+#else
+
+#ifdef _DEBUG
+	#pragma comment(lib,"QtDesignerd4")
+	#pragma comment(lib,"QtCored4")
+	#pragma comment(lib,"QtGuid4")
 #else
 #pragma comment(lib,"QtDesigner4")
 #pragma comment(lib,"QtCore4")
 #pragma comment(lib,"QtGui4")
+#endif
+
 #endif
 
  WidgetCollection::WidgetCollection(QObject *parent)
